78e527d1e352bcf406a4ae0a21e88f6dcc40a103
sadman <ahsan.susmoy@gmail.com>
2021-08-12 10:39:16 +0600

    Final Edit Done

--- Project/index.php
+++ Project/index.php
@@ -1,70 +1,153 @@
 <?php
-    $info=[
-       
-        [
-            'name'      =>  'Uzma Surwat',
-            'skill'     =>  'MERN Devs',
-            'age'       =>    '29',
-            'gender'    =>    'Female',
-            'location'  =>    'Kolkata',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'uzma.jpg'
-        ],
-        [
-            'name'      =>  'Nayan Islam',
-            'skill'     =>  'Python Devs',
-            'age'       =>    '25',
-            'gender'    =>    'Male',
-            'location'  =>    'Rajshahi',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'nayan.jpg'
-        ],
-        [
-            'name'      =>  'Tazmim Hossain Bondhon',
-            'skill'     =>  'IOS Devs',
-            'age'       =>    '24',
-            'gender'    =>    'Male',
-            'location'  =>    'Rangpur City',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'tazmim.jpg'
-        ],
-        [
-            'name'      =>  'Tasmin Ahmed',
-            'skill'     =>  'Javascript Devs',
-            'gender'    =>    'Female',
-            'age'       =>    '26',
-            'location'  =>    'Cumilla',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'tasmin.jpg'
-        ],
-        [
-            'name'      =>  'Sazia Zaman',
-            'skill'     =>  'DIVI Expert',
-            'age'       =>    '27',
-            'gender'    =>    'Female',
-            'location'  =>    'Dhaka',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'noData.png'
-        ],
-        [
-            'name'      =>  'Md. Sadman Ahsan',
-            'skill'     =>  'Wordpress Expert',
-            'age'       =>    '25',
-            'gender'    =>    'Male',
-            'location'  =>    'Dhaka',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'sadman.jpg'
-        ],
-        [
-            'name'      =>  'Arafat Rahman Talukder',
-            'skill'     =>  'JSON Expert',
-            'age'       =>    '25',
-            'gender'    =>    'Male',
-            'location'  =>    'Manikgonj',
-            'desc'      =>  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
-            'img'       =>   'arafat.jpg'
-        ]
-        ];
+$info = [
+
+    [
+        'name'     => 'Uzma Surwat',
+        'skill'    => 'MERN Devs',
+        'age'      => 29,
+        'gender'   => 'Female',
+        'location' => 'Kolkata',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'uzma.jpg',
+    ],
+    [
+        'name'     => 'Nayan Islam',
+        'skill'    => 'Python Devs',
+        'age'      => 25,
+        'gender'   => 'Male',
+        'location' => 'Rajshahi',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'nayan.jpg',
+    ],
+    [
+        'name'     => 'Tazmim Hossain Bondhon',
+        'skill'    => 'IOS Devs',
+        'age'      => 24,
+        'gender'   => 'Male',
+        'location' => 'Rangpur City',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'tazmim.jpg',
+    ],
+    [
+        'name'     => 'Tasmin Ahmed',
+        'skill'    => 'Javascript Devs',
+        'gender'   => 'Female',
+        'age'      => 26,
+        'location' => 'Cumilla',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'tasmin.jpg',
+    ],
+    [
+        'name'     => 'Sazia Zaman',
+        'skill'    => 'DIVI Expert',
+        'age'      => 24,
+        'gender'   => 'Female',
+        'location' => 'Dhaka',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'sazia.png',
+    ],
+    [
+        'name'     => 'Md. Sadman Ahsan',
+        'skill'    => 'Wordpress Expert',
+        'age'      => 25,
+        'gender'   => 'Male',
+        'location' => 'Dhaka',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'sadman.jpg',
+    ],
+    [
+        'name'     => 'Arafat Rahman Talukder',
+        'skill'    => 'JSON Expert',
+        'age'      => 25,
+        'gender'   => 'Male',
+        'location' => 'Manikgonj',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'arafat.jpg',
+    ],
+    [
+        'name'     => 'Md. Mohiuddin Sobuj',
+        'skill'    => 'Java Devs',
+        'age'      => 25,
+        'gender'   => 'Male',
+        'location' => 'Dhaka',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'mohiuddin.jpg',
+    ],
+    [
+        'name'     => 'Zubayer Rahman',
+        'skill'    => 'PHP Devs',
+        'age'      => 18,
+        'gender'   => 'Male',
+        'location' => 'Dhaka',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'Zubayer.jpg',
+    ],
+    [
+        'name'     => 'Ibrahim Robin',
+        'skill'    => 'Laravel Devs',
+        'age'      => 23,
+        'gender'   => 'Male',
+        'location' => 'Dhaka',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'Ibrahim.jpg',
+    ],
+    [
+        'name'     => 'Golam Kibria',
+        'skill'    => 'MERN Devs',
+        'age'      => 27,
+        'gender'   => 'Male',
+        'location' => 'Gazipur',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'Golam.jpg',
+    ],
+    [
+        'name'     => 'Humayun Kabir',
+        'skill'    => 'Full Stack Devs',
+        'age'      => 29,
+        'gender'   => 'Male',
+        'location' => 'Mirpur',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'Humayun.jpg',
+    ],
+    [
+        'name'     => 'Humayun Kabir',
+        'skill'    => 'Full Stack Devs',
+        'age'      => 29,
+        'gender'   => 'Male',
+        'location' => 'Dhaka',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'Humayun.jpg',
+    ],
+    [
+        'name'     => 'Jamal Hossain',
+        'skill'    => 'C++ Devs',
+        'age'      => 29,
+        'gender'   => 'Male',
+        'location' => 'Mirpur',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'Jamal.jpg',
+    ],
+    [
+        'name'     => 'HM Imu SC',
+        'skill'    => 'C# Devs',
+        'age'      => 25,
+        'gender'   => 'Male',
+        'location' => 'Mirpur',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'himu.jpg',
+    ],
+
+    [
+        'name'     => 'মো হাসিবুর রশিদ সরকার',
+        'skill'    => 'C Devs',
+        'age'      => 23,
+        'gender'   => 'Male',
+        'location' => 'Khulna',
+        'desc'     => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quae.',
+        'img'      => 'hasib.jpg',
+    ],
+   
+];
 ?>
 
 <!DOCTYPE html>
@@ -80,26 +163,26 @@
 </head>
 <body>
     <div class="container">
-    <h1 class="text-center mt-3 font-weight-bold text-danger">Team Mates</h1>
+    <h4 class="mt-3 text-center font-weight-bold text-danger">AWD CodersTrust Mates</h4>
         <div class="row">
         <?php foreach ($info as $key => $value):
-                 
-            //if($value['gender'] == 'Male'):
-            ?>
-            <div class="col-md-4 mt-4">
-                <div class="card shadow">
-                    <img class="card-img" src="assets/img/<?php echo $value['img'] ?>" style="height: 250px;" alt="">
-                    <div class="card-body">
-                        <h4 class="font-weight-bold"><?php echo $value['name'] ?></h4>
-                        <h6><span class="text-danger">Skill:</span> <?php echo $value['skill'] ?></h6>
-                        <h6><span class="text-success">Age:</span> <?php echo $value['age'] ?></h6>
-                        <h6><span class="text-info">Location:</span> <?php echo $value['location'] ?></h6>
-                        <h6><span class="text-danger">Gender:</span> <?php echo $value['gender'] ?></h6>
-                        <p><?php echo $value['desc'] ?></p>
-                    </div>
-                </div>
-            </div>
-            <?php  endforeach; ?>
+
+    //if($value['gender'] == 'Male'):
+    ?>
+		            <div class="col-md-3 mt-4 mb-4">
+		                <div class="card shadow">
+		                    <img class="card-img" src="assets/img/<?php echo $value['img'] ?>" style="height: 250px;" alt="">
+		                    <div class="card-body">
+		                        <h4 style="font-size: 16px; font-weight:bold;"><?php echo $value['name'] ?></h4>
+		                        <h6><span class="text-danger">Skill:</span> <?php echo $value['skill'] ?></h6>
+		                        <h6><span class="text-success">Age:</span> <?php echo $value['age'] ?></h6>
+		                        <h6><span class="text-info">Location:</span> <?php echo $value['location'] ?></h6>
+		                        <h6><span class="text-danger">Gender:</span> <?php echo $value['gender'] ?></h6>
+		                        <p><?php echo $value['desc'] ?></p>
+		                    </div>
+		                </div>
+		            </div>
+		            <?php endforeach;?>
         </div>
     </div>
 
