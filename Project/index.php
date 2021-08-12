337c1e2bc790eda767693018d25a62299bbe047d
Md. Sadman Ahsan <87822573+devsadman@users.noreply.github.com>
2021-08-12 14:28:35 +0600

    Update index.php
    Some Correction in js paths

--- Project/index.php
+++ Project/index.php
@@ -187,8 +187,8 @@ $info = [
     </div>
 
     <script src="assets/js/jquery-3.6.0.min.js"></script>
-    <script src="assets//js/popper.min.js"></script>
-    <script src="assets//js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     <script>
 
     </script>
